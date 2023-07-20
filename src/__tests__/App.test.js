754e119a3508b2ea4f4e3eefde14deef3aabb86c
maimuna <mohamudmaimuna003@gmail.com>
2023-07-20 11:31:50 +0300

    completed assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,23 @@
 // The code below ensures that students who are using CodeGrade will get credit
 // for the code-along in Canvas; you can disregard it.
 
-test("Test passing", () => {
-  return new Promise((resolve, reject) => {
-    resolve(true);
-  });
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+import user from "../data/user";
+import About from "../components/About";
+
+test("renders a <p> element with the bio from props", () => {
+  render(<About bio="I made this" links={user.links} />);
+  expect(screen.queryByText("I made this")).toBeInTheDocument();
+});
+
+test("does not render a <p> element if the bio is not included in props", () => {
+  const { container } = render(<About links={user.links} />);
+  expect(container.querySelector("p")).toBeNull();
+});
+
+test("does not render a <p> element if the bio is an empty string", () => {
+  const { container } = render(<About bio="" links={user.links} />);
+  expect(container.querySelector("p")).toBeNull();
 });
